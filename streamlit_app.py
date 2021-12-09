fe366cf8af7b6be6343cc0136a9f3463f2ae3096
Simon Tang <simon.tang@aboutyou.com>
2021-12-09 15:09:34 +0100

    sdmkö.sd

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,17 @@ import pandas as pd
 placeholder = st.empty()
 start_button = st.empty()
 
-def radar_chart():  
-    df = pd.DataFrame(dict(
-    r=[random.randint(0,22),
-       random.randint(0,22),
-       random.randint(0,22),
-       random.randint(0,22),
-       random.randint(0,22)],
-    theta=['processing cost','mechanical properties','chemical stability',
-           'thermal stability', 'device integration']))
-    fig = px.line_polar(df, r='r', theta='theta', line_close=True)
+def bar_chart():  
+    hours = []
+    data = []
+
+    for x in range(24):
+        hours.append(x)
+        data.append(random.randint(199019238,8237920874))
+
+    df = pd.DataFrame({'hour': hours, 'hits': data})
+    fig = px.bar(df, x='hour', y='hits')
+    # fig.show()
     placeholder.write(fig)
 
 if start_button.button('Start',key='start'):
@@ -25,7 +26,5 @@ if start_button.button('Start',key='start'):
     if st.button('Stop',key='stop'):
         pass
     while True:
-        radar_chart()
+        bar_chart()
         time.sleep(0.5)
-
-print('test')
